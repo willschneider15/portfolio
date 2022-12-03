b3278e57bcb93b014f4d65a89ff163ce31aa5878
willschneider15 <willschneider15@gmail.com>
2022-12-03 18:29:42 -0500

    updated the toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: "jit",
   // important: true,
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
