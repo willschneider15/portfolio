249ad487b506abc1ff7de4ba7c91b41b5249e5c1
willschneider15 <willschneider15@gmail.com>
2022-12-03 19:15:49 -0500

    updated the toggle

--- components/SwitchTheme.tsx
+++ components/SwitchTheme.tsx
@@ -15,26 +15,19 @@ const SwitchTheme = () => {
     body.setAttribute("data-theme", theme);
   }, [theme]);
   return (
-    // <button classNameName="btn btn-circle" onClick={toggleTheme}>
-    //   {theme === "black" ? (
-    //     <FiMoon classNameName="w-5 h-5" />
-    //   ) : (
-    //     <FiSun classNameName="w-5 h-5" />
-    //   )}
-    // </button>
-    <div className="inline-flex">
-        
-        <FiMoon className="w-5 h-5 mr-2" />
-        <label className="inline-flex relative items-center cursor-pointer">
-            
-            <input type="checkbox" value="" onClick={toggleTheme} className="sr-only peer"/>
-        
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            
-        </label>
-        <FiSun className="w-5 h-5 ml-2" />
-    </div>
-    
+    <button className="btn btn-circle" onClick={toggleTheme}>
+      {theme === "black" ? (
+        <FiMoon className="w-5 h-5" />
+      ) : (
+        <FiSun className="w-5 h-5" />
+      )}
+    </button>
+
+//     <label className="inline-flex relative items-center cursor-pointer">
+//   <input type="checkbox" value="" className="sr-only peer"/>
+//   <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+//   <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Toggle me</span>
+//     </label>
 
     
   );
