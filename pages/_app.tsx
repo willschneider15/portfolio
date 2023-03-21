110491c83695870c411aa0df82621d3a43a1c96c
willschneider15 <willschneider15@gmail.com>
2023-03-21 16:36:17 -0400

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,29 @@
 import '../styles/globals.css'
-
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
 
   
   return(
-  
- <Component {...pageProps} />
+  <>
+<Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-D4BP66BVQZ"/>
+  <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-D4BP66BVQZ', {
+        page_path: window.location.pathname,
+        });
+      `,
+    }}
+  />
+   <Component {...pageProps} />
+  </>
+
    
             
         
